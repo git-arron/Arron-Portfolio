fcbe19892f22f38ae9c978d7ab18ee3a8dac1ee2
git-arron <johnarronmojico24@gmail.com>
2025-07-30 18:39:32 +0800

    Add script.js for navbar toggle

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-button').addEventListener('click',function(){
+    const nav = document.getElementById('header-right-nav-bar');
+    nav.classList.toggle('show');
+});
